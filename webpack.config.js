b8e49877d6b376ead4260532613dae7fbe65bb61
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-05-30 12:04:38 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+	'script!jquery/dist/jquery.min.js',
+	'script!foundation-sites/dist/foundation.min.js',
+	'./app/app.jsx'
+	],
+	externals: {
+		jquery: 'jQuery',
+
+	},
+	plugins:[
+		new webpack.ProvidePlugin({
+			'$': 'jquery',
+			'jQuery': 'jquery'
+		})
+	],
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+	resolve: {
+		root: __dirname,
+		alias: {
+			Main: "app/components/Main.jsx",
+			Navigation: "app/components/Navigation.jsx",
+			applicationStyles: "app/styles/app.scss"
+		},
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015', 'stage-0']
+				},
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	},
+	devtool: 'cheap-module-eval-source-map'
+	
+	// "cheap-source-map" can be used instead of above
+	// which currently has bugs
+	// buggy warning can also be avoided with filter in console.
+	// see https://github.com/webpack/webpack/issues/91
+	// 'cheap-source-map' sort of works, but line numbers are a bit off
+	//devtool: 'cheap-source-map'
+};
