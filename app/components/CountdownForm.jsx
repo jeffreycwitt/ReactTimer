199429ec71e12ee4f9c6dc291c4d1514b5352cc3
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-06-20 19:04:08 -0400

    changes to countdownForm component and tests

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -13,10 +13,12 @@ var CountdownForm = React.createClass({
 	},
 	render: function(){
 		return(
-			<form ref="form" onSubmit={this.onSubmit} className="countdown-form">
-				<input type="text" ref="seconds" placeholder="Enter time in seconds"/>
-				<button className="button expanded">Start</button> 
-			</form>
+			<div>
+				<form ref="form" onSubmit={this.onSubmit} className="countdown-form">
+					<input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+					<button className="button expanded">Start</button> 
+				</form>
+			</div>
 			)
 	}
 });
